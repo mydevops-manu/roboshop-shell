197a7364c617910c99daa737386e76d05044b6d1
mydevops-manu <mydevops.manu@gmail.com>
2023-12-20 20:44:45 +0530

    ec2 creation script

--- roboshop.sh
+++ roboshop.sh
@@ -14,6 +14,8 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d13e6a1db95fcf83 
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d13e6a1db95fcf83 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
+    echo "$i : $i"
 done
 
