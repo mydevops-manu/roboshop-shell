a7c45c52699221e957f2ff05c90c8631521d43df
mydevops-manu <mydevops.manu@gmail.com>
2023-12-20 20:49:26 +0530

    ec2 creation script

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for  i in "${INSTANCES[@]}"
 do 
-    if [ $i -eq mongodb ] || [ $i -eq mongodb ] || [ $i -eq mongodb ]
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
