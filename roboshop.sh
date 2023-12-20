7eccbfb0f8e54a495c2471dc0b0b8b0dea975317
mydevops-manu <mydevops.manu@gmail.com>
2023-12-20 20:51:10 +0530

    ec2 creation script

--- roboshop.sh
+++ roboshop.sh
@@ -15,6 +15,6 @@ do
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d13e6a1db95fcf83 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
-    echo "$i : $i"
+    echo "$i : $IP_ADDRESS"
 done
 
