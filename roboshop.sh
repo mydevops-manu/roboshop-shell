5048bc1c65b3d29d41bc261893bc70c786dbe3e3
mydevops-manu <mydevops.manu@gmail.com>
2023-12-20 20:24:27 +0530

    ec2 creation script

--- roboshop.sh
+++ roboshop.sh
@@ -9,9 +9,9 @@ do
     echo "Instance is $i"
     if [ $i -eq mongodb ] || [ $i -eq mongodb ] || [ $i -eq mongodb ]
     then
-        $INSTANCE_TYPE="t3.small"
+        INSTANCE_TYPE="t3.small"
     else
-        $INSTANCE_TYPE="t2.micro"
+        INSTANCE_TYPE="t2.micro"
     fi
 
     aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d13e6a1db95fcf83 
