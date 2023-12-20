9e4a36b1f68b5d58f265e430f5124e7eb97cafab
mydevops-manu <mydevops.manu@gmail.com>
2023-12-20 20:17:58 +0530

    ec2 creation script

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SG_ID=sg-0d13e6a1db95fcf83
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for  i in "${INSTANCES[@]}"
+do 
+    if [ $i -eq mongodb ] || [ $i -eq mongodb ] || [ $i -eq mongodb ]
+        $INSTANCE_TYPE="t3.small";
+    else
+        $INSTANCE_TYPE="t2.micro";
+    fi
+done
+
+
+
+aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0d13e6a1db95fcf83 
